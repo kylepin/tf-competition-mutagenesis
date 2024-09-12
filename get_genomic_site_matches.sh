7e41dd744c8ef97f3201588015665fc66f114a43
Kyle Pinheiro <23409579+kylepin@users.noreply.github.com>
2024-09-12 11:30:14 -0400

    make sure conda-forge package is accessible; clean up

--- get_genomic_site_matches.sh
+++ get_genomic_site_matches.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This should be run prior to process_mutation_counts
+mkdir intermediate_and_output
 python fastaRegexFinder.py --fasta input/hg19.fa --regex "(?=(CAC(G[CAT]|AT)G))" > intermediate_and_output/myc_matches.bed
 python fastaRegexFinder.py --fasta  input/hg19.fa --regex "(?=([CAGT]((?<!C)A|[CGT])C(G[CAT]|AT)G))" > intermediate_and_output/myc_control_matches.bed
